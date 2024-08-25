04153d6708d9039eaebc7136114ebb95b8af73dc
DevKike <alvaronarvaez151@gmail.com>
2024-08-24 19:29:39 -0500

    Feat: add method for get all products

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../services/http.service';
+import { environment } from 'src/environments/environment';
+import { IFakeStoreAPIResponse } from '../interfaces/IFakeApiStore';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public products!: IFakeStoreAPIResponse;
 
-  constructor() {}
+  constructor(private readonly httpService: HttpService) {}
 
+  async ngOnInit() {
+    const url = environment.URL_BASE + 'products';
+    this.products = await this.httpService.get<IFakeStoreAPIResponse>(url);
+    console.log('🚀 ~ HomePage ~ ngOnInit ~ this.products:', this.products);
+  }
 }
